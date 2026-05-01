8368bcac410fc8147cfe4c40b4a236be95079af2
notfound405 <43387518+185264646@users.noreply.github.com>
2026-05-02 07:54:52 +0800

    finish 01_concurrency_sync/04_process_pipe

--- exercises/01_concurrency_sync/04_process_pipe/src/lib.rs
+++ exercises/01_concurrency_sync/04_process_pipe/src/lib.rs
@@ -53,7 +53,11 @@ pub fn run_command(program: &str, args: &[&str]) -> String {
     // TODO: Set stdout to Stdio::piped()
     // TODO: Execute with .output() and get output
     // TODO: Convert stdout to String and return
-    todo!()
+    let output = Command::new(program)
+        .args(args)
+        .output()
+        .unwrap();
+    String::from_utf8(output.stdout).unwrap()
 }
 
 /// Write data to child process (cat) stdin via pipe and read its stdout output.
@@ -89,7 +93,15 @@ pub fn pipe_through_cat(input: &str) -> String {
     // TODO: Write input to child process stdin
     // TODO: Drop stdin to close pipe (otherwise cat won't exit)
     // TODO: Read output from child process stdout
-    todo!()
+    let mut cmd = Command::new("cat")
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()
+        .unwrap();
+    cmd.stdin.take().unwrap().write_all(input.as_bytes()).unwrap();
+    let output = cmd.wait_with_output().unwrap();
+
+    String::from_utf8(output.stdout).unwrap()
 }
 
 /// Get child process exit code.
@@ -110,7 +122,12 @@ pub fn get_exit_code(command: &str) -> i32 {
     // TODO: Use Command::new("sh").args(["-c", command])
     // TODO: Execute and get status
     // TODO: Return exit code
-    todo!()
+    Command::new("sh")
+        .args(["-c", command])
+        .status()
+        .unwrap()
+        .code()
+        .unwrap()
 }
 
 /// Execute the given shell command and return its stdout output as a `Result`.
@@ -137,7 +154,10 @@ pub fn run_command_with_result(program: &str, args: &[&str]) -> io::Result<Strin
     // TODO: Set stdout to Stdio::piped()
     // TODO: Execute with .output() and handle Result
     // TODO: Convert stdout to String with from_utf8, mapping errors to io::Error
-    todo!()
+    let output = Command::new(program)
+        .args(args)
+        .output()?;
+    String::from_utf8(output.stdout).map_err(|e| io::Error::new(io::ErrorKind::InvalidData, "Output can not convert to utf-8 string"))
 }
 
 /// Interact with `grep` via bidirectional pipes, filtering lines that contain a pattern.
@@ -167,7 +187,17 @@ pub fn pipe_through_grep(pattern: &str, input: &str) -> String {
     // TODO: Drop stdin to close pipe
     // TODO: Read output from child stdout line by line
     // TODO: Collect and return matching lines
-    todo!()
+    let mut cmd = Command::new("grep")
+        .arg(pattern)
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()
+        .unwrap();
+
+    cmd.stdin.take().unwrap().write_all(input.as_bytes()).unwrap();
+    let output = cmd.wait_with_output().unwrap();
+
+    String::from_utf8(output.stdout).unwrap()
 }
 
 #[cfg(test)]
