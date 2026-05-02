ac18f10be00ca34db7a6e6a7b334831b89e2d970
notfound405 <43387518+185264646@users.noreply.github.com>
2026-05-03 05:11:31 +0800

    finish 03_os_concurrency/02_atomic_ordering

--- exercises/03_os_concurrency/02_atomic_ordering/src/lib.rs
+++ exercises/03_os_concurrency/02_atomic_ordering/src/lib.rs
@@ -40,7 +40,8 @@ impl FlagChannel {
     pub fn produce(&self, value: u32) {
         // TODO: Store data (choose appropriate Ordering)
         // TODO: Set ready = true (choose appropriate Ordering so data writes complete before this)
-        todo!()
+        self.data.store(value, Ordering::Relaxed);
+        self.ready.store(true, Ordering::Release);
     }
 
     /// Consumer: spin-wait for ready flag, then read data.
@@ -51,7 +52,13 @@ impl FlagChannel {
     pub fn consume(&self) -> u32 {
         // TODO: Spin-wait for ready to become true (choose appropriate Ordering)
         // TODO: Read data (choose appropriate Ordering)
-        todo!()
+        loop {
+            let ready = self.ready.load(Ordering::Acquire);
+
+            if ready {
+                break self.data.load(Ordering::Relaxed);
+            }
+        }
     }
 
     /// Reset channel state
@@ -83,13 +90,31 @@ impl OnceCell {
     pub fn init(&self, val: u32) -> bool {
         // TODO: Use compare_exchange to ensure initialization only once
         // Store value on success
-        todo!()
+        loop {
+            let init = self.initialized.load(Ordering::Acquire);
+
+            if init {
+                return false;
+            }
+
+            self.value.store(val, Ordering::Relaxed);
+
+            match self.initialized.compare_exchange(init, true, Ordering::AcqRel, Ordering::Acquire) {
+                Err(_) => (),
+                Ok(_) => {
+                    break true;
+                }
+            }
+        }
     }
 
     /// Get value. Returns Some if initialized, otherwise None.
     pub fn get(&self) -> Option<u32> {
         // TODO: Check initialized flag, then read value
-        todo!()
+        match self.initialized.load(Ordering::Acquire) {
+            true => Some(self.value.load(Ordering::Relaxed)),
+            false => None,
+        }
     }
 }
 
